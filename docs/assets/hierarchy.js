927f026666a592a33c8a9c454e208a21b02218e1
farfromrefug <dev@akylas.fr>
2025-01-07 09:49:31 +0100

    readme/doc

--- docs/assets/hierarchy.js
+++ docs/assets/hierarchy.js
@@ -1,1 +1,1 @@
-window.hierarchyData = "eJyrVirKzy8pVrKKjtVRKkpNy0lNLsnMzwMKVNfWAgCbHgqm"
+window.hierarchyData = "eJyVjTELwjAUhP/Lza9KJIO8zW5u7qVDSCIJpmnJS6eS/y6lk4OIyw133/FtKPNcBTxoPRKKfyZva5yzgDeoPbKZPBj3fFuWx1psMOJBeMXswOpyJawlgWGTEfFy/gBPoU4JdIxgVHHd/uyOohG0/ibp/xH1v2UEG2JyxWfwoMbW2hvBNFTF"
